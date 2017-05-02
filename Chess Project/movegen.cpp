897ce3519685c538dee1753dbf691f9e057c844a
kimh8399 <kimh8399@googleapps.wrdsb.ca>
2017-05-02 08:27:00 -0400

    calcMove
    Added calcMove function

--- Chess Project/movegen.cpp
+++ Chess Project/movegen.cpp
@@ -3,12 +3,12 @@
 #include "movegen.h"
 #include "bitboards.h"
 
-typedef vector<Piece> pvec;
-
+typedef vector<attacked> avec;
+/*
 generate_move(pieceBoard Board, colour which_colour)
 {
-    /*
-    pvec attack_loc[8][8];
+
+    avec attack_loc[8][8];
     for(int i = 0; i < 8; i++)
     {
         for(int j = 0; j < 8; j++)
@@ -16,9 +16,24 @@ generate_move(pieceBoard Board, colour which_colour)
             if(pieceBoard.board[i][j].attack_option.attack_coord.size() != 0)
             {
                 for(int t = 0; t < pieceBoard.board[i][j].attack_option.attack_coord.size(); t++)
-                    attack_loc[pieceBoard.board[i][j].attack_option.attack_coord[t].x][pieceBoard.board[i][j].attack_option.attack_coord[t].y].push_back(pieceBoard.board[i][j]);
+                    attack_loc[i][j].attack_option.attack_coord;
             }
 
         }
-    }*/
+    }
+
 }
+*/
+void calcMoves(pieceBoard &board, bitboard attackBoard, colour side)
+{
+    vector <int> moves;
+    for (int i=0;i<8;i++)
+    {
+        for (int j=0;j<8;j++)
+        {
+            if(board.board[i][j].side==side)
+                board.board[i][j].moves(board, attackBoard);
+        }
+    }
+}
+
